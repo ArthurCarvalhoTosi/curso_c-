01038de27ff30eedd030a13143985af8a16d0bb1
Arthur Tosi <arthurtosi07@hotmail.com>
2024-07-02 18:42:04 -0300

    Metodos

--- Teste5/Teste5/Triangulo.cs
+++ Teste5/Teste5/Triangulo.cs
@@ -11,5 +11,16 @@ namespace Teste5
         public double A;
         public double B;
         public double C;
+        public double p;
+        public double raiz;
+
+        public double CalcularArea()
+        {
+         
+            p = (A + B + C) / 2.0;
+            raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            return raiz;
+            
+        }
     }
 }
