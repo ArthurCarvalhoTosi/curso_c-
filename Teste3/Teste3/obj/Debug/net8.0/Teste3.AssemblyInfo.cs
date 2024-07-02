219210d5ffd19f768f1f322ce79965ff0aa3290d
Arthur Tosi <arthurtosi07@hotmail.com>
2024-07-01 21:26:29 -0300

    Classes, Objetos e Atributos

--- Teste3/Teste3/obj/Debug/net8.0/Teste3.AssemblyInfo.cs
+++ Teste3/Teste3/obj/Debug/net8.0/Teste3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38051b1f40c77201d0aaeaf5fb7611343ae5e585")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Teste_1/Teste_1/obj/Release/net8.0/Teste_1.AssemblyInfo.cs
+++ Teste_1/Teste_1/obj/Release/net8.0/Teste_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Teste_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38051b1f40c77201d0aaeaf5fb7611343ae5e585")]
 [assembly: System.Reflection.AssemblyProductAttribute("Teste_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Teste_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
