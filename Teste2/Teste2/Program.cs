38051b1f40c77201d0aaeaf5fb7611343ae5e585
Arthur Tosi <arthurtosi07@hotmail.com>
2024-07-01 21:12:01 -0300

    first commit

---
+++ Teste2/Teste2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace Teste2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um int, char e double, respectivamente:");
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double d = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(d.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Digite um nome, sexo (m ou f), idade e altura, respectivamente na mesma linha separados por ',':");
+            string[] vet = Console.ReadLine().Split(',');
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);   
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2"));
+        }
+    }
+}
---
+++ Teste_1/Teste_1/Program.cs
@@ -0,0 +1,30 @@
+﻿class Teste
+{
+    static void Main(string[] args)
+    {
+        List<char> vogais = new List<char> { 'a','e', 'i', 'o', 'u' };
+
+        string banana = "banana";
+
+        int quantidadeDeVogais = banana.Count(vogais.Contains);
+
+        IEnumerable<char> vogaisUnicas = banana.Distinct().Where(vogais.Contains);
+
+        Console.WriteLine($"A palavra {banana} contém {quantidadeDeVogais} vogais");
+        Console.WriteLine("As vogais que contém são:");
+        foreach(char vogal in vogaisUnicas)
+        {
+            Console.WriteLine(vogal);
+        }
+
+        string?[] vet = Console.ReadLine().Split(' ');
+
+        string p1 = vet[0];
+        string p2 = vet[1];
+        string p3 = vet[2];
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+        Console.WriteLine(p3);
+    }
+}
